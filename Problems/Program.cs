422b5739899804c3cb724b00b0de890ae80893e4
marginboo <g18m6734@HUG77>
2020-02-24 17:46:19 +0200

    Part1 Q12

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -4,9 +4,21 @@ namespace Problems
 {
     class Program
     {
+        public static void Prefix (string input)
+        {
+            int ln = input.Length;
+            string[] mum = input.Split();
+            int sis = mum.Length;
+            Console.WriteLine(ln.ToString(), ",",sis.ToString() , " ", input);
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter something");
+            string man = Console.ReadLine();
+            Program mn = new Program();
+            Prefix(man);
+            
         }
     }
 }
